c4b31e0cef281f4445806b7750f766f4a24ce184
obidiallah <bbedoogamer@gmail.com>
2024-12-06 17:01:35 +0300

    Minor adjustments
    Added different backgrounds, player scripts and suffering with weird camera problems

---
+++ Scripts/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+const SPEED = 400
+const JUMP_VELOCITY = -350
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+@onready var sprite = $AnimatedSprite2D
+
+func player_direction(direction):
+		#changes direction of char
+	if direction > 0:
+		sprite.flip_h = false
+		#attacks_hitbox.scale.x = 1
+	elif direction < 0:
+		sprite.flip_h = true
+		#attacks_hitbox.scale.x = -1
+
+func _physics_process(delta):
+	var direction = Input.get_axis("left","right")
+	
+	#makes spirte turn
+	player_direction(direction)
+	
+	#pulls player to the ground
+	if not is_on_floor():
+		velocity.y += (gravity * 1.25) * delta
+	
+	#player movement
+	if is_on_floor():
+		if !velocity:
+			sprite.play("default")
+		else:
+			sprite.play("walk")
+	#elif !is_on_floor():
+	#	sprite.play("Jump")
+	
+	velocity.x = direction * SPEED
+	
+	move_and_slide()
